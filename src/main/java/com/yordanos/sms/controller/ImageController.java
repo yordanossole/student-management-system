18b065e52cfe44467f915d265b2f44470f1b5305
Yordanos Solomon <yordanos.sole@gmail.com>
2024-10-17 11:44:46 +0300

    bug on getting image by id fixed

--- src/main/java/com/yordanos/sms/controller/ImageController.java
+++ src/main/java/com/yordanos/sms/controller/ImageController.java
@@ -1,11 +1,17 @@
 package com.yordanos.sms.controller;
 
+import com.yordanos.sms.exception.ResourceNotFoundException;
 import com.yordanos.sms.model.Image;
-import com.yordanos.sms.response.ApiResponse;
 import com.yordanos.sms.service.ImageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
 @RequestMapping("/image")
@@ -13,9 +19,20 @@ import org.springframework.web.bind.annotation.*;
 public class ImageController {
     private final ImageService imageService;
 
-    @GetMapping("/id/{id}")
-    public ResponseEntity<ApiResponse> getImageById(@PathVariable Long id) {
-        Image image = imageService.getImageById(id);
-        return ResponseEntity.ok(new ApiResponse("Found", image.getId()));
+    @GetMapping("/view/id/{id}")
+    public ResponseEntity<Resource> getImageById(@PathVariable Long id) throws SQLException {
+        try {
+            Image image = imageService.getImageById(id);
+            if (image != null) {
+                ByteArrayResource resource = new ByteArrayResource(image.getImageFile().getBytes(1, (int) image.getImageFile().length()));
+                return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getFilename() + "\"")
+                        .body(resource);
+            } else {
+                throw new ResourceNotFoundException("Image with id " + id + " not found");
+            }
+        } catch (ResourceNotFoundException | SQLException e) {
+            return ResponseEntity.status(NOT_FOUND).body(null);
+        }
     }
 }
